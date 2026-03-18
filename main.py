02ad89926439f632bb2d331442b45d02efad315c
justxexe <faizov.dmitry@gmail.com>
2026-03-18 16:32:52 +0300

    initial commit 2

--- main.py
+++ main.py
@@ -1,1 +1,80 @@
+import pygame
 
+from src.player import Player
+
+class App:
+    def __init__(self):
+        self.player_sprite = None
+        self._running = True
+        self.screen = None
+        self.size = self.weight, self.height = 900, 640
+        self.clock = pygame.time.Clock()
+        self.delta_time = None
+        self.entities = []
+
+    def on_init(self):
+        pygame.init()
+        self.entities.append(Player((0,0)))
+
+        self.screen = pygame.display.set_mode(self.size)
+        self._running = True
+        self.delta_time = 0.1
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pygame.display.flip()
+        self.delta_time = self.clock.tick(60) / 1000
+
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if not self.on_init():
+            self._running = True
+
+
+        while self._running:
+            self.screen.fill((20, 20, 20))
+            for event in pygame.event.get():
+                self.on_event(event)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.entities[0].up_pressed = True
+                    if event.key == pygame.K_s:
+                        self.entities[0].down_pressed = True
+                    if event.key == pygame.K_a:
+                        self.entities[0].right_pressed = True
+                    if event.key == pygame.K_d:
+                        self.entities[0].left_pressed = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.entities[0].up_pressed = False
+                    if event.key == pygame.K_s:
+                        self.entities[0].down_pressed = False
+                    if event.key == pygame.K_a:
+                        self.entities[0].right_pressed = False
+                    if event.key == pygame.K_d:
+                        self.entities[0].left_pressed = False
+
+                self.entities[0].update(self.delta_time)
+
+
+            for entity in self.entities:
+                self.screen.blit(entity.sprite, entity.position)
+
+                entity.draw(self.screen)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
--- player.py
+++ player.py
@@ -1,1 +1,42 @@
+import pygame
 
+class Player():
+    def __init__(self, position):
+        self.size_scale = 0.1
+        self.sprite = pygame.image.load("./resources/олень.jpg")
+        self.sprite = pygame.transform.scale(self.sprite,
+                                                    (self.sprite.get_width() * self.size_scale,
+                                                     self.sprite.get_height() * self.size_scale))
+        self.velocity = 5
+
+        self.position = list(position)
+        self.hitbox = pygame.Rect(self.position[0], self.position[1], self.sprite.get_width(), self.sprite.get_height())
+
+        self.up_pressed = False
+        self.down_pressed = False
+        self.right_pressed = False
+        self.left_pressed = False
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255,0,0), self.hitbox)
+
+    def update(self, delta):
+        velX = 0
+        velY = 0
+        if self.up_pressed and not self.down_pressed:
+            velY -= self.velocity
+        if self.down_pressed and not self.up_pressed:
+            velY += self.velocity
+        if self.right_pressed and not self.left_pressed:
+            velX -= self.velocity
+        if self.left_pressed and not self.right_pressed:
+            velX += self.velocity
+
+        if velX != 0 and velY != 0:
+            velX = velX * 0.707
+            velY = velY * 0.707
+
+
+        self.position[0] += velX
+        self.position[1] += velY
+        self.hitbox = pygame.Rect(self.position[0], self.position[1], self.sprite.get_width(), self.sprite.get_height())
